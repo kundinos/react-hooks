db339e8a941bda2fdb35c2a46c10e1d6f4ddeb14
Yury Kundin <yury.kundin@yandex.ru>
2020-11-22 00:28:34 +0300

    Сделал экспорт всех хуков

--- src/index.ts
+++ src/index.ts
@@ -1,2 +1,5 @@
 export { default as useBem } from './useBem';
+export { default as useFetch } from './useFetch';
+export { default as useGlobalState } from './useGlobalState';
 export { default as useInterval } from './useInterval';
+export { default as useWindowEvent } from './useWindowEvent';
