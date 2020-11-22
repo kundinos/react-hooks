3395b6dc9867b0c82e7d6f92dbcc93f7e49018a3
Yury Kundin <yury.kundin@yandex.ru>
2020-11-22 07:26:54 +0300

    Добавил экспорт useTimeout

--- src/index.ts
+++ src/index.ts
@@ -3,4 +3,5 @@ export { default as useFetch } from './useFetch';
 export { default as useFullState } from './useFullState';
 export { default as useGlobalState } from './useGlobalState';
 export { default as useInterval } from './useInterval';
+export { default as useTimeout } from './useTimeout';
 export { default as useWindowEvent } from './useWindowEvent';
