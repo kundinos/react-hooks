c1d4b260b883ec359b1342055c67af9a775fea29
Yury Kundin <yury.kundin@yandex.ru>
2020-11-22 06:10:19 +0300

    Изменил настройки JEST

--- jest.config.ts
+++ jest.config.ts
@@ -3,7 +3,11 @@ import type { Config } from '@jest/types';
 const config: Config.InitialOptions = {
   verbose: true,
   automock: false,
-  setupFiles: ['./setupJest.ts'],
+  setupFiles: ['../setupJest.ts'],
+  collectCoverage: true,
+  collectCoverageFrom: ['**/*.{ts,tsx}'],
+  coverageDirectory: 'coverage',
+  rootDir: 'src',
 };
 
 export default config;
