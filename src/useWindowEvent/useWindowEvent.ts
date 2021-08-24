4e50288d6fc4545f28614f0d5e0e65e80ce68b17
Yury Kundin <yury.kundin@yandex.ru>
2021-08-25 00:58:50 +0300

    feature: Move typings to separate file

--- src/useWindowEvent/useWindowEvent.ts
+++ src/useWindowEvent/useWindowEvent.ts
@@ -1,14 +1,6 @@
 import { useEffect } from 'react';
 
-export interface UseWindowEventOptions extends AddEventListenerOptions {
-  initial?: boolean;
-}
-
-export type UseWindowEvent = <K extends keyof WindowEventMap>(
-  type: K,
-  listener: (this: Window, ev: WindowEventMap[K]) => void,
-  options?: boolean | UseWindowEventOptions,
-) => void;
+import { UseWindowEvent } from './typings';
 
 const useWindowEvent: UseWindowEvent = (type, listener, options) => {
   useEffect(() => {
