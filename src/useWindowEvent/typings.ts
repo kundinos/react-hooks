13f92ba709ac581089d4c80f2501843dccc34ef0
Yury Kundin <yury.kundin@yandex.ru>
2021-08-28 20:09:03 +0300

    docs: Add description  for hook

--- src/useWindowEvent/typings.ts
+++ src/useWindowEvent/typings.ts
@@ -1,5 +1,10 @@
 import { UseNativeEventOptions } from '../useNativeEvent';
 
+/**
+ * Simplifies the subscribing to events on window. Deletes the subscription after unmount component
+ *
+ * @see https://kundinos.ru/react-hooks/useWindowEvent
+ */
 export type UseWindowEvent = <K extends keyof WindowEventMap>(
   type: K,
   listener: EventListener,
