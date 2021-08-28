c811e4a2b26f383f36ca38eae81e03df2872b228
Yury Kundin <yury.kundin@yandex.ru>
2021-08-28 20:08:17 +0300

    docs: Add description for hook

--- src/useToggle/typings.ts
+++ src/useToggle/typings.ts
@@ -1,1 +1,8 @@
+/**
+ * Returns a state with true/false value and function for his toggle
+ *
+ * @param initialState The initial state for the switch
+ *
+ * @see https://kundinos.ru/react-hooks/useToggle
+ */
 export type UseToggle = (initialState?: boolean | (() => boolean)) => [boolean, () => void];
