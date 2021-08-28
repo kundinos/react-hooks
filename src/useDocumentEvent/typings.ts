33524cdc64b1f209768ad5a74cd533d06febfb92
Yury Kundin <yury.kundin@yandex.ru>
2021-08-28 19:40:03 +0300

    docs: Add description for hook

--- src/useDocumentEvent/typings.ts
+++ src/useDocumentEvent/typings.ts
@@ -1,5 +1,9 @@
 import { UseNativeEventOptions } from '../useNativeEvent';
-
+/**
+ * Simplifies the subscribing to events on document. Deletes the subscription after unmount component
+ *
+ * @see https://kundinos.ru/react-hooks/useDocumentEvent
+ */
 export type UseDocumentEvent = <K extends keyof DocumentEventMap>(
   type: K,
   listener: EventListener,
