8a87ccae2c4964eb0ea83025483b732118e3d06b
Yury Kundin <yury.kundin@yandex.ru>
2021-08-28 19:52:11 +0300

    style: Remove useless comments

--- src/useGlobalState/useGlobalState.ts
+++ src/useGlobalState/useGlobalState.ts
@@ -5,12 +5,10 @@ const store = { state: undefined, initialState: undefined, listeners: [] };
 const useGlobalState = <T>(initialState?: T): [T, Dispatch<SetStateAction<T>>] => {
   const [state, setState] = useState<T>(store.state || initialState);
 
-  //
   const isCurrentListener = useCallback((listener) => {
     return listener === setState;
   }, []);
 
-  //
   useEffect(() => {
     store.initialState = store.initialState || initialState;
     store.state = state;
@@ -18,7 +16,6 @@ const useGlobalState = <T>(initialState?: T): [T, Dispatch<SetStateAction<T>>] =
     store.listeners.forEach((listener) => !isCurrentListener(listener) && listener(state));
   }, [initialState, isCurrentListener, state]);
 
-  //
   useEffect(() => {
     store.listeners.push(setState);
 
