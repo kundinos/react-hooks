5f5054d98e47a6b7d5544486aaec55b2ce4d8fa9
Yury Kundin <yury.kundin@yandex.ru>
2021-08-28 16:56:52 +0300

    docs: Add docs for useDate

--- src/useDate/typings.ts
+++ src/useDate/typings.ts
@@ -4,4 +4,11 @@ export interface UseDateOptions {
   every: UseDatePeriod;
 }
 
-export type UseDate = (opts?: UseDateOptions) => Date;
+/**
+ * Returns a stateful current date, that will update every specified period
+ *
+ * @param options.every The period updating the date. Possible values: millisecond, second (by default), minute, hour, day or custom number of milliseconds
+ *
+ * @see https://kundinos.ru/react-hooks/useDate
+ */
+export type UseDate = (options?: UseDateOptions) => Date;
