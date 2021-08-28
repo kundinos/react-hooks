b243ca7917ad008e1bcb41cda1ecf5ad43a39c76
Yury Kundin <yury.kundin@yandex.ru>
2021-08-28 19:44:47 +0300

    docs: Add description for hook

--- src/useFullState/typings.ts
+++ src/useFullState/typings.ts
@@ -1,3 +1,8 @@
 import { Dispatch, SetStateAction } from 'react';
 
+/**
+ * Works as useState, but contains third parameter with previous value of state
+ *
+ * @see https://kundinos.ru/react-hooks/useFullState
+ */
 export type UseFullState = <S>(initialState?: S | (() => S)) => [S, Dispatch<SetStateAction<S>>, S];
