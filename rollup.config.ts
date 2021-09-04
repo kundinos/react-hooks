8360f342590def1e9612ae287b6439c1c08ff6fc
Yury Kundin <yury.kundin@yandex.ru>
2021-09-04 14:02:33 +0300

    feature: Improve the bundling of library
    - Bundle d.ts files to one file
    - Disable source maps
    - Minify source code with terser

--- rollup.config.ts
+++ rollup.config.ts
@@ -2,32 +2,45 @@ import typescript from 'rollup-plugin-typescript2';
 import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
 import resolve from 'rollup-plugin-node-resolve';
+import dts from 'rollup-plugin-dts';
+import { terser } from 'rollup-plugin-terser';
 
 import pkg from './package.json';
 
-export default {
-  input: 'src/index.ts',
-  output: [
-    {
-      file: pkg.main,
-      format: 'cjs',
-      exports: 'named',
-      sourcemap: true,
-    },
-    {
-      file: pkg.module,
-      format: 'es',
-      exports: 'named',
-      sourcemap: true,
-    },
-  ],
-  plugins: [
-    external(),
-    resolve(),
-    typescript({
-      rollupCommonJSResolveHack: true,
-      clean: true,
-    }),
-    commonjs(),
-  ],
-};
+export default [
+  {
+    input: 'src/index.ts',
+    output: [
+      {
+        file: pkg.main,
+        format: 'cjs',
+        exports: 'named',
+      },
+      {
+        file: pkg.module,
+        format: 'es',
+        exports: 'named',
+      },
+    ],
+    plugins: [
+      external(),
+      resolve(),
+      typescript({
+        rollupCommonJSResolveHack: true,
+        clean: true,
+        tsconfigOverride: {
+          compilerOptions: {
+            declaration: false,
+          },
+        },
+      }),
+      commonjs(),
+      terser(),
+    ],
+  },
+  {
+    input: './src/index.ts',
+    output: [{ file: './dist/index.d.ts', format: 'es' }],
+    plugins: [dts()],
+  },
+];
