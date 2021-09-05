7e64da03f13df63d844d6668ddca2fede090b98d
Yury Kundin <yury.kundin@yandex.ru>
2021-09-05 13:19:06 +0300

    feature: Add conditions when user inactive

--- src/useIdle/useIdle.ts
+++ src/useIdle/useIdle.ts
@@ -1,25 +1,56 @@
-import { useCallback, useState } from 'react';
+import { useCallback, useRef, useState } from 'react';
 
 import useDocumentEvent from '../useDocumentEvent';
+import useTimeout, { UseTimeoutResult } from '../useTimeout';
 import { UseIdle } from './typings';
 
 const useIdle: UseIdle = (options = {}) => {
   const { onIdle, onWakeup } = options;
   const [idle, setIdle] = useState(false);
+  const timeout = useRef(options.timeout || 3000);
+  const timer = useRef<UseTimeoutResult>();
+
+  const emitOnIdle = useCallback(
+    (e?) => {
+      if (idle) return;
+
+      timer.current.reset();
+      setIdle(true);
+      if (onIdle) onIdle(e);
+    },
+    [idle, onIdle],
+  );
+
+  const emitOnWakeup = useCallback(
+    (e) => {
+      if (!idle) return;
+
+      timer.current.reset();
+      setIdle(false);
+      if (onWakeup) onWakeup(e);
+    },
+    [idle, onWakeup],
+  );
 
   const handleVisibilityChange = useCallback(
     (e) => {
       const isHidden = document.visibilityState === 'hidden';
 
-      setIdle(isHidden);
-
-      if (onIdle && isHidden) onIdle(e);
-      if (onWakeup && !isHidden) onWakeup(e);
+      if (isHidden) {
+        emitOnIdle(e);
+      } else {
+        emitOnWakeup(e);
+      }
     },
-    [onIdle, onWakeup],
+    [emitOnIdle, emitOnWakeup],
   );
 
   useDocumentEvent('visibilitychange', handleVisibilityChange);
+  useDocumentEvent('click', emitOnWakeup);
+  useDocumentEvent('keydown', emitOnWakeup);
+  useDocumentEvent('mousemove', emitOnWakeup);
+
+  timer.current = useTimeout(emitOnIdle, timeout.current);
 
   return idle;
 };
