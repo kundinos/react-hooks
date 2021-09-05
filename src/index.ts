13107b20f065786b71445d6e78fb4ff8baf5360c
Yury Kundin <yury.kundin@yandex.ru>
2021-09-05 13:42:44 +0300

    feature: Add export for useIdle

--- src/index.ts
+++ src/index.ts
@@ -3,6 +3,7 @@ export { default as useDocumentEvent } from './useDocumentEvent';
 export { default as useFetch } from './useFetch';
 export { default as useFullState } from './useFullState';
 export { default as useGlobalState } from './useGlobalState';
+export { default as useIdle } from './useIdle';
 export { default as useInterval } from './useInterval';
 export { default as useKeyboardEvents } from './useKeyboardEvents';
 export { default as useNativeEvent } from './useNativeEvent';
