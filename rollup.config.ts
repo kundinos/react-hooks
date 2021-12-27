603c82518008019afb2cb74ece3faf998112ad2b
Yury Kundin <yury.kundin@yandex.ru>
2021-12-27 21:09:38 +0300

    chore: Update the dependencies

--- rollup.config.ts
+++ rollup.config.ts
@@ -1,7 +1,7 @@
+import resolve from '@rollup/plugin-node-resolve';
+import commonjs from '@rollup/plugin-commonjs';
 import typescript from 'rollup-plugin-typescript2';
-import commonjs from 'rollup-plugin-commonjs';
 import external from 'rollup-plugin-peer-deps-external';
-import resolve from 'rollup-plugin-node-resolve';
 import dts from 'rollup-plugin-dts';
 import { terser } from 'rollup-plugin-terser';
 
